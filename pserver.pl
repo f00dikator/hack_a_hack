07df368aab8985824d04b32e33c786e762f2d062
John Lampe <dmitry.chan@gmail.com>
2021-04-14 14:21:51 -0400

    -sql and -xss cli switches
    add in -sql and -xss switches to force FUZZ variables to those classes

--- pserver.pl
+++ pserver.pl
@@ -21,6 +21,18 @@ require("$incfile");
 
 $req = calculate_req();
 
+foreach $arg (@ARGV)
+{
+    if ($arg eq "-sql")
+    {
+        @goofy = (";", "--", "\/*", "#", "'", '"');
+    }
+    if ($arg eq "-xss")
+    {
+        @goofy = ("\<", "\>", "'", '"');
+    }
+}
+
 if ($udp) {
     do_udp();
     exit(0);
@@ -34,36 +46,36 @@ die "No port. $!\n" unless $port;
 
 socket(soc, PF_INET, SOCK_STREAM, 0 ) ||  die "socket failed on $port. $!\n";
 
-setsockopt (soc, SOL_SOCKET, SO_REUSEADDR, pack('l', 1)) ||  die "setsockopt failed on $port. $!\n"; 
+setsockopt (soc, SOL_SOCKET, SO_REUSEADDR, pack('l', 1)) ||  die "setsockopt failed on $port. $!\n";
 
-bind (soc, sockaddr_in($port, INADDR_ANY)) ||  die "can't bind to $port. $!\n"; 
+bind (soc, sockaddr_in($port, INADDR_ANY)) ||  die "can't bind to $port. $!\n";
 
 listen (soc, SOMAXCONN) || die "can't listen on $port. $!\n";
 
 print "Listening on Port $port\n";
 
 while ($cli = accept(client,soc) ) {
-	$sleepycounter=0;
+        $sleepycounter=0;
         next if $pid = fork;
         die "\n\nERROR: frikkin fork on $port. $!\n\n" unless defined $pid;
         close(soc);
          $blastcount=0;
          $blastsize = length($req);
-         while (defined(client)) 
-	 {                           
+         while (defined(client))
+         {
             $blastcount++;
             $total = $blastcount * $blastsize;
-	    if ($blastcount > 1)
-	    {
-	    	$req = calculate_req();
-		if ( ($sleepytime == 1) && ($sleepycounter == 0) )
-		{
-			sleep(2);
-			$sleepycounter++;
-		}
-            	$blast = send (client, $req, 0);
+            if ($blastcount > 1)
+            {
+                $req = calculate_req();
+                if ( ($sleepytime == 1) && ($sleepycounter == 0) )
+                {
+                        sleep(2);
+                        $sleepycounter++;
+                }
+                $blast = send (client, $req, 0);
                 print "\[$blastcount\]:  $total bytes -> $port \n";
-	    }
+            }
         }
 } continue {
         close(client);
@@ -77,11 +89,11 @@ sub do_udp {
                                     Proto => "udp") || die "\n\nERROR: Can't serve udp on $port. $!\n\n";
     $blastcount=0;
     $blastsize = length($req);
-    while ($server->recv($datagram, 64)) 
+    while ($server->recv($datagram, 64))
     {
         $blastcount++;
         $total = $blastcount * $blastsize;
-        $server->send($req);  
+        $server->send($req);
         print "\[$blastcount\]:  $total bytes -> $port \n";
     }
 }
@@ -89,36 +101,36 @@ sub do_udp {
 
 
 sub calculate_req
-{	
-	# if @goofy defined in cnf file, then use it
-	if ($#goofy <= 0)
-	{
-		@goofy = qw(< & ' " >);
-	}
-	$index = $#goofy;
-	@timerray = localtime;
-	$seconds = @timerray[0];
-	$seconds++;
-	$entropy = $seconds * 3;
-	$mcount = 0;
+{
+        # if @goofy defined in cnf file, then use it
+        if ($#goofy <= 0)
+        {
+                @goofy = qw(< & ' " >);
+        }
+        $index = $#goofy;
+        @timerray = localtime;
+        $seconds = @timerray[0];
+        $seconds++;
+        $entropy = $seconds * 3;
+        $mcount = 0;
 
-	for ($zztop=0; length($fuzzrray[$zztop]) >= 1; $zztop++)
-	{
+        for ($zztop=0; length($fuzzrray[$zztop]) >= 1; $zztop++)
+        {
             if ($fuzzrray[$zztop] =~ /FUZZ/)
             {
-			$insert = "";
-			for ($ff=0; $ff < $entropy; $ff++)
-			{
-				$span = 300;
-  				$random = int(rand($span));
-				$insert .= $goofy[($entropy + $mcount + $random) % $index ];
-				$mcount++;
-			}
-			$myreq = $myreq . $insert;
+                        $insert = "";
+                        for ($ff=0; $ff < $entropy; $ff++)
+                        {
+                                $span = 300;
+                                $random = int(rand($span));
+                                $insert .= $goofy[($entropy + $mcount + $random) % $index ];
+                                $mcount++;
+                        }
+                        $myreq = $myreq . $insert;
             } else {
                 $myreq = $myreq . $fuzzrray[$zztop];
             }
-	}
+        }
 
-	return ($myreq);
+        return ($myreq);
 }
